f98c5145a7e622e863384b6bcf1e662986059aea
Veloprocess <Gabriel.araujo@velotax.com.br>
2025-08-20 09:58:57 -0300

    update db and front

--- api/ask.js
+++ api/ask.js
@@ -3,7 +3,7 @@
 import { google } from 'googleapis';
 
 // --- CONFIGURAÇÃO ---
-const SPREADSHEET_ID = "1tyS89983odMbF04znYTmDLO6_6nZ9jFs0kz1fkEjnvY";
+const SPREADSHEET_ID = "1tnWusrOW-UXHFM8GT3o0Du93QDwv5G3Ylvgebof9wfQ";
 const FAQ_SHEET_NAME = "FAQ!A:C"; // Colunas: Pergunta, Resposta, Palavras-chave
 const CACHE_DURATION_SECONDS = 300;
 
