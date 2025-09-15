016c043de34441d00f4b33045fa7b94a52e4377f
Gabriel lima de araujo <Gabriel.araujo@velotax.com.br>
2025-09-15 12:28:50 -0300

    update config

--- api/config.js
+++ api/config.js
@@ -1,10 +1,19 @@
 // config.js
+
+import 'dotenv/config';
+
+export default function handler(req, res) {
+  res.status(200).json({
+    clientId: process.env.CLIENT_ID,
+    dominioPermitido: process.env.DOMINIO_PERMITIDO
+  });
+}
+
+
 export const ARQUIVOS_AUTORIZADOS = [
   "./data/politicas_internas.txt",
   "./data/manuais_operacionais.txt"
 ];
-
-
 
 export const SITES_AUTORIZADOS = [
   "https://intranet.empresa.com.br",
